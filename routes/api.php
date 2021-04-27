3150f8172f1030c45e680228d071db60b4a124db
632nkn <632nkn@gmail.com>
2021-04-27 17:52:21 +0900

    first_commit

---
+++ routes/api.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+
+//Laravel８式書き方
+use App\Http\Controllers\TaskController;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::middleware('auth:api')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+//自作API
+Route::get('/tasks', [TaskController::class, 'index']);
+Route::get('/tasks/{task}', [TaskController::class, 'show']);
+Route::post('/tasks', [TaskController::class, 'store']);
+Route::put('/tasks/{task}', [TaskController::class, 'update']);
+Route::delete('/tasks/{task}', [TaskController::class, 'destroy']);
